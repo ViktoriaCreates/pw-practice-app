2241f2df76941fcbf6ec59daf3af62b3737bc83d
ViktoriaCreates <viktoriiacolozzo@gmail.com>
2024-04-13 19:07:18 -0400

    Modify file locators-syntax.spec.ts

--- tests/locators-syntax.spec.ts
+++ tests/locators-syntax.spec.ts
@@ -8,22 +8,31 @@ test.beforeEach(async({page}) => {
 })
 
 test('Locator syntax rules', async({page}) => {
+
 // by Tag name
 page.locator('input')
+
 // by ID
 page.locator('#inputEmail1')
+
 // by Class value
 page.locator('.shape-rectangle')
+
 // by Attribute
 page.locator('[placeholder="Email"]')
+
 // by Class value (full)
 page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]')
+
 // by Combining different selectors
 page.locator('input[placeholder="Email"][nbinput]') 
+
 // by XPath (NOT RECOMMENDED with PLAYWRIGHT)
 page.locator('//*[id="inputEmail1"]')
+
 // by Partial text match
 page.locator(':text("Using")')
+
 // by Exact text match
 page.locator(':text-is("Using the Grid")')
 })
