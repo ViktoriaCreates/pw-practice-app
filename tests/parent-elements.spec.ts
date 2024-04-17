a6cf16eb837ee352f9c1bdce09853f5bd099bed6
ViktoriaCreates <viktoriiacolozzo@gmail.com>
2024-04-16 20:14:49 -0400

    Add file parent-elements with a branch parent-elements

---
+++ tests/parent-elements.spec.ts
@@ -0,0 +1,21 @@
+import {test} from '@playwright/test'
+
+//hook
+test.beforeEach(async({page}) => {
+    await page.goto('http://localhost:4200/')
+    await page.getByText('Forms').click()
+    await page.getByText('Form Layouts').click()
+})
+
+test('locating parent elements', async({page}) => {
+    await page.locator('nb-card', {hasText: "Using the Grid"}).getByRole('textbox', {name: "Email"}).click()
+    await page.locator('nb-card', {has: page.locator('#inputEmail1')}).getByRole('textbox', {name: "Email"}).click()
+
+    await page.locator('nb-card').filter({hasText: "Basic form"}).getByRole('textbox', {name: "Email"}).click()
+    await page.locator('nb-card').filter({has: page.locator('.status-danger')}).getByRole('textbox', {name: "Password"}).click()
+
+    await page.locator('nb-card').filter({has: page.locator('nb-checkbox')}).filter({hasText: "Sign in"})
+        .getByRole('textbox', {name: "Email"}).click()
+
+    await page.locator(':text-is("Using the Grid")').locator('..').getByRole('textbox', {name: "Email"}).click()    
+})
